20ef2f4a015dadf3ec0fa5d4072923c93d829876
Raghu Betina <raghu@firstdraft.com>
2017-02-24 02:56:25 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "cities#index"
   # Routes for the Crossfit_box resource:
   # CREATE
   get "/crossfit_boxes/new", :controller => "crossfit_boxes", :action => "new"
